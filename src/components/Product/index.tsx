a97b8edbbebff5c147d9f58b15e24447bb34df91
Nelson Jr <juniordell123@gmail.com>
2020-09-01 22:25:19 -0300

    add Product component

---
+++ src/components/Product/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import tshirtImage from '../../assets/tshirt.png'
+
+import { Container } from './styles'
+
+const Product: React.FC = () => {
+  return (
+    <Container>
+      {/* <Row>
+        <a href='#'>Compartilhar</a>
+        <a href='#'>Vender um igual</a>
+      </Row>
+
+      <Panel>
+        <Column>
+          <Gallery>
+            <img src={tshirtImage} alt='T-shirt' />
+          </Gallery>
+        </Column>
+
+        <Column></Column>
+      </Panel> */}
+    </Container>
+  )
+}
+
+export default Product
